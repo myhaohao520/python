1bd1aaf958e6c967a52de1bb44fa564a6fbff1d6
LazyTang <50754881+myhaohao520@users.noreply.github.com>
2023-03-17 14:14:31 -0700

    Create areas.py

---
+++ areas.py
@@ -0,0 +1,13 @@
+#calculate the area of different geometrical figures
+
+#! /usr/bin/env python3
+import math
+
+def triangle(base, height):
+  return base*height/2
+
+def rectangle(base, height):
+  return base*height
+
+def circle(radius):
+  return math.pi*radius**2
